211e8b3b24f8e9ceab2bd35383fc5f4e4285053d
Davide <davide.vavassori@gmail.com>
2024-01-06 22:28:45 +0100

    added add todo function

--- gui.py
+++ gui.py
@@ -2,9 +2,20 @@ import functions
 import PySimpleGUI as psg
 
 label = psg.Text("Type in a todo")
-inputBox = psg.InputText(tooltip="Enter todo")
+inputBox = psg.InputText(tooltip="Enter todo",key="todo")
 add_button = psg.Button("Add")
 
-window = psg.Window("My to-do List", layout=[[label], [inputBox,add_button]])
-window.read()
+window = psg.Window("My to-do List",
+                    layout=[[label], [inputBox, add_button]],
+                    font=("Helvetica", 20))
+while True:
+    event,value = window.read()
+    match event:
+        case "Add":
+            todos = functions.getTodos()
+            new_todo = value["todo"] + '\n'
+            todos.append(new_todo)
+            functions.writeTodos(todos)
+        case psg.WIN_CLOSED:
+            break
 window.close()
