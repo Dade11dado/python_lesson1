7a926e0835a0423fcdef2bcb95879d49f195a2e7
Davide <davide.vavassori@gmail.com>
2024-01-09 16:33:49 +0100

    Implement 'complete new todo'

--- web.py
+++ web.py
@@ -3,24 +3,26 @@ import functions
 
 todos = functions.getTodos()
 
+
 def add_todo():
-    todo = st.session_state["new_todo"] +'\n'
+    todo = st.session_state["new_todo"] + '\n'
     todos.append(todo)
     functions.writeTodos(todos)
     st.session_state["new_todo"] = ""
-
-
 
 
 st.title("My todo app")
 st.subheader("This is my todo app")
 st.write("This is for s web developement app")
 
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.writeTodos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-
-for todo in todos:
-    st.checkbox(todo)
-
-st.text_input(label="",placeholder="Add new todo",
+st.text_input(label="Enter todo", placeholder="Add new todo",
               on_change=add_todo,
               key="new_todo")
